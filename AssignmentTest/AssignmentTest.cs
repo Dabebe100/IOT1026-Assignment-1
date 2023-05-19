988d366fa3f37845e23a980abf943bdaf0f7194d
Dabebe100 <130933203+Dabebe100@users.noreply.github.com>
2023-05-19 15:07:50 +0100

    Update AssignmentTest.cs

--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -1,5 +1,4 @@
 ﻿using Assignment;
-
 // Write tests ONLY for the ReplicateArray method. This method should not need any Console.ReadLine/Console.WriteLine statements.
 // You should have at least three test cases
 // 1. Empty Array (Done for you)
@@ -7,7 +6,6 @@
 // 3. Array with multiple elements
 // Ensure the copied array has the same elements as the original array (CollectionAssert.AreEqual)
 // Ensure that the new array is deep copied (Assert.AreNotSame)
-
 namespace AssignmentTest
 {
     [TestClass]
@@ -16,12 +14,38 @@ namespace AssignmentTest
         [TestMethod]
         public void ReplicateArray_EmptyArray_ReturnsEmptyArray()
         {
-            /*
             int[] original = Array.Empty<int>();
             int[] result = ArrayReplicator.ReplicateArray(original);
             CollectionAssert.AreEqual(original, result);
             Assert.AreNotSame(original, result);
-            */
         }
+
+        [TestMethod]
+        public void ReplicateArray_ArrayWithElements()
+        {
+            int[] original = new int[] { 1, 2, 3 };
+            int[] result = ArrayReplicator.ReplicateArray(original);
+            CollectionAssert.AreEqual(original, result);
+            Assert.AreNotSame(original, result);
+        }
+
+        [TestMethod]
+        public void ReplicateArray_ArrayWithNegativeElements()
+        {
+            int[] original = new int[] { -1, 2, -3, 0, -999, 100120 };
+            int[] result = ArrayReplicator.ReplicateArray(original);
+            CollectionAssert.AreEqual(original, result);
+            Assert.AreNotSame(original, result);
+        }
+        /* Great idea -> But since we take in int[] and not int[]? 
+        * it is the user's responsiblity to make sure the array is not null
+        public void ReplicateArray_NullArray_ThrowsException()
+        {
+            int[] original = null;
+            int[] result = Array.Empty
+            CollectionAssert.AreEqual(original, result);
+            Assert.AreNotSame(original, result);
+        }
+        */
     }
 }
